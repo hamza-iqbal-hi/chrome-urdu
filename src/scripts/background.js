604473d69153023848a8a6942fe48a29c465b19a
hamza-iqbal-hi <hamzaiqbalwc@gmail.com>
2024-01-23 20:27:06 +0500

    remove console logs

--- src/scripts/background.js
+++ src/scripts/background.js
@@ -1,11 +1,8 @@
 const key = "cu-font";
 
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-  console.log("GOT A MESSAGE", chrome);
   if (message.action === "cu-save-font") {
-    chrome.storage.local.set({ [key]: message.font }, function () {
-      console.log("Data saved to local storage");
-    });
+    chrome.storage.local.set({ [key]: message.font }, function () {});
   } else if (message.action === "cu-get-font") {
     chrome.storage.local.get([key], function (result) {
       sendResponse({ font: result.key });
