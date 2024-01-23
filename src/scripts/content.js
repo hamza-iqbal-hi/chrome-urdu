66c884032ca9405b8591c0b2e9c960f5c359ea07
hamza-iqbal-hi <hamzaiqbalwc@gmail.com>
2024-01-23 21:33:15 +0500

    update fonts url

--- src/scripts/content.js
+++ src/scripts/content.js
@@ -18,14 +18,14 @@ const injectFonts = () => {
    
     @font-face {
         font-family: "sameer-mosan";
-        src: url(https://github.com/hamza-iqbal-hi/chrome-urdu/blob/main/fonts/sameer-mosan.ttf?raw=true) format("truetype");
+        src: url(https://github.com/hamza-iqbal-hi/chrome-urdu/blob/main/public/fonts/sameer-mosan.ttf?raw=true) format("truetype");
         font-style: normal;
         font-weight: 400;
         unicode-range: U+0600-06FF, U+0750-077F, U+0870-088E, U+0890-0891, U+0898-08E1, U+08E3-08FF, U+200C-200E, U+2010-2011, U+204F, U+2E41, U+FB50-FDFF, U+FE70-FE74, U+FE76-FEFC;
       }
       @font-face {
         font-family: "gulzar";
-        src: url(https://github.com/hamza-iqbal-hi/chrome-urdu/blob/main/fonts/gulzar.ttf?raw=true) format("truetype");
+        src: url(https://github.com/hamza-iqbal-hi/chrome-urdu/blob/main/public/fonts/gulzar.ttf?raw=true) format("truetype");
         font-style: normal;
         font-weight: 400;
         unicode-range: U+0600-06FF, U+0750-077F, U+0870-088E, U+0890-0891, U+0898-08E1, U+08E3-08FF, U+200C-200E, U+2010-2011, U+204F, U+2E41, U+FB50-FDFF, U+FE70-FE74, U+FE76-FEFC;
