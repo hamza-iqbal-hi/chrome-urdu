35cae565b257418fa9ec6a2fe926161bbf6ebe27
hamza-iqbal-hi <hamzaiqbalwc@gmail.com>
2024-01-23 20:01:35 +0500

    add popup UI

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -3,10 +3,12 @@ document
   .addEventListener("click", function () {
     const selectedFont = document.getElementById("fontSelector").value;
 
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, {
+    chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+      const activeTab = tabs[0];
+      chrome.tabs.sendMessage(activeTab.id, {
         action: "changeFont",
         font: selectedFont,
+        message: "start",
       });
     });
   });
