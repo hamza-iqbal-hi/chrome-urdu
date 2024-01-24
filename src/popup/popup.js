a811ae303b76ad8981b1a3f8ebb49e22162f975b
hamza-iqbal-hi <hamzaiqbalwc@gmail.com>
2024-01-24 14:08:11 +0500

    add fonts to stylesheet

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -2,7 +2,6 @@ document
   .getElementById("cu-change-font")
   .addEventListener("click", function () {
     const selectedFont = document.getElementById("fontSelector").value;
-
     chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
       const activeTab = tabs[0];
       chrome.tabs.sendMessage(activeTab.id, {
